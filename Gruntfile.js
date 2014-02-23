bb2a91b0951aa80464d825b3f8b6a2bce5544a8d
R.A. Lucas <richard@richardalucas.com>
2014-02-23 13:04:59 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
+    grunt.loadNpmTasks('grunt-cafe-mocha');
+    grunt.loadNpmTasks('grunt-env');
+
+    grunt.initConfig({
+        
+        env: {
+            test: { NODE_ENV: 'TEST' },
+            coverage: { NODE_ENV: 'COVERAGE' }
+        },
+
+        cafemocha: {
+            test: {
+                src: 'test/*.js',
+                options: {
+                    ui: 'bdd',
+                    reporter: 'spec'
+                },
+            },
+            coverage: {
+                src: 'test/*.js',
+                options: {
+                    ui: 'bdd',
+                    reporter: 'html-cov',
+                    coverage: {
+                        output: 'coverage.html'
+                    }
+                }
+            }
+        },
+
+        mocha_istanbul: {
+            coverage: {
+                src: 'test', // the folder, not the files,
+                options: {
+                    mask: '*.js'
+                }
+            },
+            coveralls: {
+                src: 'test', // the folder, not the files
+                options: {
+                    coverage: true
+                }
+            }
+        }
+
+    });
+
+    grunt.registerTask('test', [ 'env:test',
+        'cafemocha:test' ]);
+
+    grunt.registerTask('coverage', [ 'env:coverage',
+        'mocha_istanbul:coverage',
+        'cafemocha:coverage' ]);
+
+    grunt.registerTask('coveralls', [ 'env:coverage',
+        'mocha_istanbul:coveralls',
+        'cafemocha:coverage' ]);
+
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+Basic Express setup following Advanced Express Web Development book
+---
+
+Uses:
+- nconf for configuration
+- winston for logging
+- grunt for task automation
+- mocha and supertest for testing
+- istanbul for test coverage
+
+Nicely modularized and can be potentially used as a boilerplate/starting point
